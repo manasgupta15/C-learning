0c1dee9d4cd607746f7238f897070c8aa4771da2
MANAS GUPTA <120665112+manasgupta15@users.noreply.github.com>
2024-02-19 19:45:40 +0530

    Add files via upload

---
+++ decimalToBinaary2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int ans = 0;
+    int i = 0;
+
+    // Handle the sign bit
+    int sign = (n < 0) ? -1 : 1;
+    n = abs(n);
+
+    while(n != 0) {
+        int bit = n % 2;
+        ans = bit * pow(10, i) + ans;
+
+        n = n / 2;
+        i++;
+    }
+
+    // Include the sign bit in the binary representation
+    ans = sign * ans;
+
+    cout << "Binary representation is: " << ans << endl;
+
+    return 0;
+}
