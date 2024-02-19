6d5f912e45a9c319ef32bd4c5c7d414be098c226
MANAS GUPTA <120665112+manasgupta15@users.noreply.github.com>
2024-02-19 19:47:13 +0530

    Update decimalToBinaary2.cpp

--- decimalToBinaary2.cpp
+++ decimalToBinaary2.cpp
@@ -14,10 +14,10 @@ int main()
     n = abs(n);
 
     while(n != 0) {
-        int bit = n % 2;
+        int bit = n & 1;
         ans = bit * pow(10, i) + ans;
 
-        n = n / 2;
+        n = n >> 1;
         i++;
     }
 
